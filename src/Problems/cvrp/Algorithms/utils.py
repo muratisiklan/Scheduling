f89a932d8ed44cfe44db73221c086f23f6a1f68a
muratisiklan <muratisiklan@hotmail.com>
2023-12-19 14:28:04 +0300

    calculate cost completed

--- src/Problems/cvrp/Algorithms/utils.py
+++ src/Problems/cvrp/Algorithms/utils.py
@@ -2,37 +2,41 @@ def calculate_total_cost(solution, distances, demands, vehicle_capacities):
     total_cost = 0
     visited_customers = set()
 
-    current_vehicle = 0  
+    current_vehicle = 0
+    total_loads = []  # Indicates total load of each single vehicles
+
 
     for vehicle_route in solution:
         if not vehicle_route:
             current_vehicle += 1
-            continue  
+            continue
 
-        current_capacity = 0
-        current_node = 0  
+        current_load = 0
+        current_node = 0
 
         for customer in vehicle_route:
-            
+
             if customer in visited_customers:
-                total_cost += float('inf') 
+                total_cost += float('inf')
             else:
-               
+
                 distance_to_customer = distances[current_node][customer]
 
                 total_cost += distance_to_customer
 
                 current_node = customer
 
-                current_capacity += demands[customer]
+                current_load += demands[customer]
 
                 visited_customers.add(customer)
 
+        total_loads.append(current_load)
+
         total_cost += distances[current_node][0]
 
-        if current_capacity > vehicle_capacities[current_vehicle]:
-            total_cost += float('inf')  
+        if current_load > vehicle_capacities[current_vehicle]:
+            total_cost += float('inf')
 
-        current_vehicle += 1  
+        current_vehicle += 1
 
-    return total_cost
+    return total_cost, total_loads
